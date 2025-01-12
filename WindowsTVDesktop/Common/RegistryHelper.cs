4e212255bb131a3b94af59848a9028787daba1ff
肖强 <546247083@qq.com>
2025-01-12 14:56:12 +0800

    开机自启动

---
+++ WindowsTVDesktop/Common/RegistryHelper.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Win32;
+
+namespace WindowsTVDesktop.Common
+{
+    /// <summary>
+    /// 注册表帮助类
+    /// </summary>
+    public static class RegistryHelper
+    {
+        /// <summary>
+        /// 设置key
+        /// </summary>
+        /// <param name="keyPath"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool Set(string keyPath, string key, string value, RegistryKey topKey = null)
+        {
+            if (topKey == null)
+            {
+                topKey = Registry.CurrentUser;
+            }
+
+            using (var rk = topKey.OpenSubKey(keyPath, true))
+            {
+                if (rk == null)
+                {
+                    return false;
+                }
+
+                rk.SetValue(key, value);
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// 删除注册表信息
+        /// </summary>
+        /// <param name="keyPath"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static bool Del(string keyPath, string key, RegistryKey topKey = null)
+        {
+            if (topKey == null)
+            {
+                topKey = Registry.CurrentUser;
+            }
+
+            using (var rk = topKey.OpenSubKey(keyPath, true))
+            {
+                if (rk == null)
+                {
+                    return true;
+                }
+
+                rk.DeleteValue(key, false);
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// 获取注册表信息
+        /// </summary>
+        /// <param name="keyPath"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static (bool, object) Get(string keyPath, string key, RegistryKey topKey = null)
+        {
+            if (topKey == null)
+            {
+                topKey = Registry.CurrentUser;
+            }
+
+            using (var rk = topKey.OpenSubKey(keyPath, true))
+            {
+                if (rk == null)
+                {
+                    return (false, string.Empty);
+                }
+
+                var obj = rk.GetValue(key);
+                if (obj == null)
+                {
+                    return (false, string.Empty);
+                }
+
+                return (true, obj);
+            }
+        }
+    }
+}
--- WindowsTVDesktop/ViewModels/TaskbarIconViewModel .cs
+++ WindowsTVDesktop/ViewModels/TaskbarIconViewModel .cs
@@ -1,11 +1,44 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.Windows;
+using WindowsTVDesktop.Common;
 
 namespace WindowsTVDesktop.ViewModels
 {
     public class MainTaskbarIconViewModel : ObservableObject
     {
+        public MainTaskbarIconViewModel()
+        {
+            ReLoad();
+        }
+
+        #region 界面属性
+
+        /// <summary>
+        /// 自启动
+        /// </summary>
+        private bool autoStart;
+
+        /// <summary>
+        /// 自启动
+        /// </summary>
+        public bool AutoStart
+        {
+            get
+            {
+                return autoStart;
+            }
+            set
+            {
+                autoStart = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region 界面方法
+
         /// <summary>
         /// 点击通知按钮
         /// </summary>
@@ -17,7 +50,7 @@ namespace WindowsTVDesktop.ViewModels
         /// <param name="commandParameterEx">参数</param>
         private void Click(object? type)
         {
-            if (type == null) 
+            if (type == null)
             {
                 return;
             }
@@ -36,7 +69,55 @@ namespace WindowsTVDesktop.ViewModels
             }
             else if (type.ToString() == "AutoStart")
             {
+                UpdateIsLaunchOnSysPowerOnByTaskScheduler();
             }
         }
+
+        #endregion
+
+        #region 私有方法
+
+        /// <summary>
+        /// 获取是否计划任务自启动
+        /// </summary>
+        /// <returns></returns>
+        private bool GetIsLaunchOnSysPowerOnByTaskScheduler()
+        {
+            var taslScheduler = TaskSchedulerHelper.Get(AppGlobal.AppName);
+
+            return taslScheduler != null;
+        }
+
+        /// <summary>
+        /// 更新是否计划任务自启动
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">e</param>
+        private void UpdateIsLaunchOnSysPowerOnByTaskScheduler()
+        {
+            // 修改是否自启动
+            var currentValue = !GetIsLaunchOnSysPowerOnByTaskScheduler();
+            if (currentValue)
+            {
+                TaskSchedulerHelper.AddLuanchTask(AppGlobal.AppName,System.Windows.Forms.Application.ExecutablePath);
+            }
+            else
+            {
+                TaskSchedulerHelper.Del(AppGlobal.AppName);
+            }
+
+            ReLoad();
+        }
+
+        #endregion
+
+        #region 公共方法
+
+        public void ReLoad()
+        {
+            AutoStart = GetIsLaunchOnSysPowerOnByTaskScheduler();
+        }
+
+        #endregion
     }
 }
